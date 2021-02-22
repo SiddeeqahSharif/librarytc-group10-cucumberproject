0c26d6999069a82edb6966225e695b3642f39447
Siddeeqah Sharif Fichman <74164209+SiddeeqahSharif@users.noreply.github.com>
2021-02-21 19:01:39 -0500

    added Failed Test Runner

---
+++ src/test/java/com/cybertek/library/page/LoginPage.java
@@ -0,0 +1,46 @@
+package com.cybertek.library.page;
+
+import com.cybertek.library.utilities.ConfigurationReader;
+import com.cybertek.library.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "inputEmail")
+    public WebElement inputUsername;
+
+    @FindBy(id = "inputPassword")
+    public WebElement inputPassword;
+
+    @FindBy(className = "btn btn-lg btn-primary btn-block")
+    public WebElement loginButton;
+
+    //login method where we dont have to pass username, password
+    public void loginToLibrary(){
+        inputUsername.sendKeys("student48@library");
+        inputPassword.sendKeys("xjapSRzD");
+        loginButton.click();
+    }
+
+    //overloaded version where we can pass username and password
+    public void loginToLibrary(String username, String password){
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+        loginButton.click();
+    }
+
+    //login method where we can pass data from configuration.properties
+    public void loginToLibrary_Config(){
+
+        inputUsername.sendKeys(ConfigurationReader.getProperty("Username"));
+        inputPassword.sendKeys(ConfigurationReader.getProperty("Password"));
+        loginButton.click();
+
+    }
+}
