8dd3f36190b3e9157835c298f60aeeeb49231afb
emildahalic109 <74035959+emildahalic109@users.noreply.github.com>
2021-02-26 01:47:30 -0500

    just added plugin in cukes runner,so now we can use cucumber html report ;)

--- src/test/java/com/cybertek/library/runners/CukesRunner.java
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -7,6 +7,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin={
+                "html:target/cucumber-rport.html"
+        },
         features = "src/test/resources/features",
         glue = "com/cybertek/library/step_definitions",
         dryRun = false,
